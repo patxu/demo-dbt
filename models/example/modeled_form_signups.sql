6a77f44aed9c2f67fde7340057fe57cfc4330182
Kevin Tran <kebin@hightouch.io>
2022-08-12 06:35:22 +0000

    fix copy

--- models/example/modeled_form_signups.sql
+++ models/example/modeled_form_signups.sql
@@ -1,19 +1,4 @@
 
-/*
-    This is a data model that pulls user data from all sources (Customer 360)
-    If you need access to all users captured across Sales, Marketing, Support, and Product.
-*/
-
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
 
 /*
     This is a data model that pulls user data from all sources (Customer 360)
