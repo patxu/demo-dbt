4c6ecd2195d350df56e2091a20de08f62c95c7a1
Kevin Tran <kebin@hightouch.io>
2023-07-11 20:12:22 +0000

    s

--- models/example/all_users.sql
+++ models/example/all_users.sql
@@ -14,6 +14,31 @@ with source_data as (
 
 )
 
+
+
+SELECT ROW_NUMBER() OVER (ORDER BY RANDOM(1)) AS PRIMARY_KEY,
+       ROW_NUMBER() OVER (ORDER BY RANDOM(1)) || '-' || EMAIL AS EMAIL,
+t.user_id,
+t.first_name,
+t.last_name,
+CEIL(engagement_last_7_days/4) AS "DASHBOARD_VIEWS",
+t.vip_customer AS "TRIAL_USER",
+t.vip_customer,
+t.engagement_last_7_days,
+DATEADD(hours, -engagement_last_7_days, CURRENT_DATE()) AS "SIGN_UP_DATE",
+CEIL(engagement_last_7_days/4) AS "NUM_BLOG_POSTS_VIEWED",
+CEIL(engagement_last_7_days/3) AS "PAGEVIEWS",
+t.brand_affinity AS "FAVORITE_BRAND",
+t.vip_customer AS "AD_CONVERSION",
+CEIL(engagement_last_7_days/2) AS "LEAD_SCORE",
+CEIL(engagement_last_7_days/3) AS "LEAD_GRADE",
+t.vip_customer AS "ACTIVE_POC"
+from PRISTINE_ECOMMERCE.PUBLIC.USERS t
+CROSS JOIN (
+  VALUES (1), (2), (3), (4), (5), (6)
+) AS multiplier (repeat)
+
+
 -- select
 --   user_id,
 --   first_name,
@@ -32,10 +57,6 @@ with source_data as (
 --   CEIL(engagement_last_7_days/3) AS "LEAD_GRADE",
 --   vip_customer AS "ACTIVE_POC"
 -- from PRISTINE_ECOMMERCE.PUBLIC.USERS
-
-
-
-select * from PRISTINE_ECOMMERCE.PUBLIC.mulitplied_users
 
 
 /*
