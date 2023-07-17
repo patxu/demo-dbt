7eb8da5f09fd0bf25dee208546a9e5370a6c0306
Kevin Tran <kebin@hightouch.io>
2023-07-17 19:09:52 +0000

    "reverting change" - unnecessary casting

--- models/example/all_users.sql
+++ models/example/all_users.sql
@@ -17,7 +17,6 @@ with source_data as (
 
 
 SELECT ROW_NUMBER() OVER (ORDER BY RANDOM(1)) AS PRIMARY_KEY,
-       CAST(ROW_NUMBER() OVER (ORDER BY RANDOM(1)) AS varchar) AS PRIMARY_KEY_STR,
        ROW_NUMBER() OVER (ORDER BY RANDOM(1)) || '-' || EMAIL AS EMAIL,
 t.user_id,
 t.first_name,
